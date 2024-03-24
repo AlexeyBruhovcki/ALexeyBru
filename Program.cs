573448ae568610b5899d06e1835ca4999d44c156
Alexey Bruhovecki <164076425+AlexeyBruhovcki@users.noreply.github.com>
2024-03-24 19:44:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Тема 6. Урок 1. Преобразованияя типов и способы.
+
+string num = "28";
+byte numByte = byte.Parse(num); 
+nint numNint = (nint)numByte;
+int numInt = (int)numNint;
+long numLong = (long)numInt;
+double numDouble = (double)numLong;
+
+Console.Write($" byte: {numByte}\n nint: {numNint}\n" +
+    $" int: {numInt}\n long: {numLong}\n double: {numDouble}");
+
+
+
+
+
