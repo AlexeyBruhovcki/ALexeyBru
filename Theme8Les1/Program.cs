662e8aab46de8d29f6729c0d5c10850ed838a3e2
a a <doejayne20@gmail.com>
2024-03-31 21:32:20 +0200

    Домашняя работа

---
+++ Theme8Les1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Тема 8. Урок 1 задание 1.
+
+int bookCount = 10;
+Console.Write($"Петя взял {bookCount} книг\n");
+bookCount -= 4;
+Console.Write($"Алеша взял {bookCount} книг\n");
+bookCount += 2;
+Console.Write($"Ваня взял {bookCount} книг\n");
+Console.Write($"Вместе они зяли {10 +(10-4)+(10-4+2)} книги\n");
+
---
+++ Theme8Les2Ex3/Program.cs
@@ -0,0 +1,5 @@
+﻿// Тема8. Урок2. Задание3.
+int potato = 44;
+potato /= 4;
+potato *= 2;
+Console.WriteLine($"Повар потратил {potato} кг лука");
