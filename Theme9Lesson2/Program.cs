140c0def4d9188ac5d746ab825dd4d79dd928cb3
a a <doejayne20@gmail.com>
2024-04-06 12:04:56 +0200

    HW

---
+++ Theme9Lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Тема 9. Урок 2.
+//Задание 1
+Console.WriteLine("Задание1");
+long x = ~264895468854;
+long y = ~256;
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+//Задание 2
+Console.WriteLine("Задание2");
+Console.WriteLine($"Результат операции сдвига вправо числа 256: {256>>1}");
+Console.WriteLine($"Результат операции сдвига влево числа 256: {256 << 1}");
+Console.WriteLine($"Результат операции сдвига вправо числа 32856: {32856 >> 1}");
+Console.WriteLine($"Результат операции сдвига влево числа 32856: {32856 << 1}");
+//Задание 3
+Console.WriteLine("Задание3");
+int a = 289;
+int b = 144;
+int c = 36274;
+a += 2487;
+b += 2487;
+c -= 2487;
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+Console.WriteLine($"c = {c}");
+
