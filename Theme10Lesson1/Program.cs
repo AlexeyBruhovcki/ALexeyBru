ced63fa65f5ce52e8f42f4bf40795745116ab579
a a <doejayne20@gmail.com>
2024-04-06 12:18:33 +0200

    HW

---
+++ Theme10Lesson1/Program.cs
@@ -0,0 +1,5 @@
+﻿//Тема 10. Урок1.
+Console.WriteLine($"Введите радиус окружности: ");
+double r = double.Parse(Console.ReadLine());
+Console.WriteLine($"Длина окружности равна: {2 * Math.PI * r:F2}\n" +
+    $"Площадь окружности равна: {Math.PI * r * r:F2}");
---
+++ Theme10Lesson2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Тема 10. Урок 2.
+Console.Write("Введите значение катета: ");
+double a=double.Parse(Console.ReadLine());
+Console.Write("Введите значение гипотенузы: ");
+double c = double.Parse(Console.ReadLine());
+Console.WriteLine($"Значение гипотенузы: {Math.Sqrt(c*c-a*a):F2}");
