12f9f01a0e26860fd2e863c260708d955673d3b3
a a <doejayne20@gmail.com>
2024-04-10 16:06:18 +0200

    hw

---
+++ Theme11Lesson2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Тема 11. Урок2.
+Console.Write("Введите температуру воздуха: ");
+double temp=double.Parse(Console.ReadLine());
+if (temp>20) Console.WriteLine("На прогулку можно выпустить слонов, жирафов, львов");
+else if (temp<20) Console.WriteLine("На прогулку можно выпустить медведей, волков, пингвинов");
+else if (temp==20) Console.WriteLine("На прогулку можно выпустить кенгуру");
+else Console.WriteLine("температура не входит в допустимый диапазон");
