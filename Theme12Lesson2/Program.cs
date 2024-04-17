7fe7e31e20bd12166994018e92de51f50bf416e7
a a <doejayne20@gmail.com>
2024-04-17 11:48:36 +0200

    hw

---
+++ Theme12Lesson2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Тема 12. Урок 2.
+Console.WriteLine("Выберите один из ароматов:\n" +
+    "1-Сhanel\n" +
+    "2-Dior\n" +
+    "3-Tom Ford\n" +
+    "4-Gucci\n" +
+    "5-Yves Saint Lauren\n");
+
+int n =int.Parse(Console.ReadLine());
+switch (n)
+{
+    case 1: Console.WriteLine("Аромат жасмина, розы, сандала и ванили."); break;
+    case 2: Console.WriteLine("Фруктовые аккорды с древесными нотами."); break;
+    case 3: Console.WriteLine("Ноты лимона, ромашки, кориандра, базилика, ладанника, коричного дерева, кардамона."); break;
+    case 4: Console.WriteLine("Белоцветочный, древесно-мускусный, фруктовый и слегка цитрусовый аромат."); break;
+    case 5: Console.WriteLine("Верхние ноты мандарина, чёрной смородины и зелёного красного перца.");break;
+}
---
+++ Theme13Lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Тема 13.Урок1
+Random random = new Random();
+int randNum = random.Next(1,6);
+int i = 1;
+Console.WriteLine(randNum);
+while (i <= 3)
+{
+    Console.WriteLine("Введите целое число от 1 до 5: ");
+    int userNum = int.Parse(Console.ReadLine());
+    if (userNum == randNum) 
+    {
+        Console.WriteLine("Победа");
+        break;
+    } 
+   
+    else Console.WriteLine($"Поражение, осталось попыток: {3 - i}"); i++;
+}
+Console.WriteLine($"Компьютер загадал число {randNum}");
+
