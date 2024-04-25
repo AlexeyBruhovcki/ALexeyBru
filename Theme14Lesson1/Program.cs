0f77cb9f15be8875c83b5e02ee8a808f25ca34c7
a a <doejayne20@gmail.com>
2024-04-25 08:00:55 +0200

    hw

--- Theme14Lesson1/Program.cs
+++ Theme14Lesson1/Program.cs
@@ -4,14 +4,13 @@ int lenght = int.Parse(Console.ReadLine());
 string letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 string num = "0123456789";
 string passWord = "";
-Random random1 = new Random();
-Random random2 = new Random();
-Random random3 = new Random();
+
+Random random = new Random();
 for (int i = 1;i<= lenght;i++)
 {
-    int n1 = random1.Next(1,3);
-    int n2 = random2.Next(52);
-    int n3 = random3.Next(10);
+    int n1 = random.Next(1,3);
+    int n2 = random.Next(52);
+    int n3 = random.Next(10);
     if (n1 == 1)
     {
         passWord += letters[n2];
