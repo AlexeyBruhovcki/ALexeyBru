283a06ecdc93b208358cb5dbda6e8fa138839a58
a a <doejayne20@gmail.com>
2024-04-29 11:46:40 +0200

    HW

---
+++ Theme15Lesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Тема 15.Урок1
+string[] words = {"освоении","по плечу","сложный","но если","программирования",
+    "многие бросают","остальное будет","освоить их","этап в","на этом этапе","Массивы"};
+for (int i = 0; i < words.Length; i++)
+{
+    Console.WriteLine((i + 1)+ " "+words[i]);
+}
+Console.WriteLine();
+string[] phrase=new string[words.Length];
+for (int i = 0; i < phrase.Length; i++)
+{
+    Console.Write($"Введите номер элемента из списка по по порядку следования в предложении: ");
+    int n = int.Parse(Console.ReadLine())-1;
+    phrase[i]+=words[n];
+}
+Console.WriteLine();
+Console.WriteLine("Итоговая фраза:");
+for (int i = 0; i < phrase.Length; i++)
+{
+    Console.Write(phrase[i] + " ");
+}
+Console.WriteLine();
---
+++ Theme15Lesson2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Тема 15. Урок 2
+int[] num=new int[10];
+Random random = new Random();
+for (int i = 0; i < num.Length; i++)
+{
+    num[i] = random.Next(10, 100);
+    Console.Write(num[i] + " ");
+}
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] % 2 == 0) count +=1;
+}
+Console.WriteLine();
+Console.WriteLine("Число четных чисел: "+ count);
