334a63badc3c52d49e02773095d2aed712cc5a42
a a <doejayne20@gmail.com>
2024-05-08 14:05:58 +0200

    HW

---
+++ Theme16Lesson1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Тема 16. Урок 1.
+//Задание 1.
+//Random random = new Random();
+//double[,] mas = new double[3, 3];
+//for(int i = 0; i < mas.GetLength(0); i++)
+//{
+//    for (int j = 0; j< mas.GetLength(1); j++)
+//    {
+//        mas[i, j] = Math.Round(random.NextDouble() * 20, 2);
+//        Console.Write(mas[i, j]+" ");
+//    }
+//    Console.WriteLine();
+//}
+//Console.WriteLine();
+//Console.Write("Выберите число для замены: ");
+//double num = double.Parse(Console.ReadLine());
+//Console.Write("Число, которым хотите его заменить: ");
+//double numReplace = double.Parse(Console.ReadLine());
+//for (int i = 0; i < mas.GetLength(0); i++)
+//{
+//    for (int j = 0; j < mas.GetLength(1); j++)
+//    {
+//        if (mas[i, j] == num)
+//        {
+//            mas[i, j] = numReplace;
+//        }
+//    }
+//}
+//Console.WriteLine();
+//for (int i = 0; i < mas.GetLength(0); i++)
+//{
+//    for (int j = 0; j < mas.GetLength(1); j++)
+//    {
+
+//        Console.Write(mas[i, j]+" ");
+//    }
+//    Console.WriteLine();
+//}
+//Console.WriteLine();
+//Задание 2.
+char[,] mas =
+{
+    {'т','и'},
+    {'а','с'},
+    {'в','м'},
+    {'у','к'},
+    {'б','н'},
+};
+Console.Write($"{mas[0, 0]}{mas[1, 0]}{mas[3, 1]}{mas[1, 1]}{mas[0,1]}");
+
+
---
+++ Theme16Lesson2/Program.cs
@@ -0,0 +1,71 @@
+﻿// Тема16. Урок 2.
+
+using System;
+
+char[,] mas =
+{
+    {' ',' ','X','X',' ',' '},
+    {' ','X',' ',' ','X',' '},
+    {'X',' ',' ',' ',' ','X'},
+    {' ','X','X','X','X',' '},
+    {' ',' ','X','X',' ',' '},
+    {' ',' ','X','X',' ',' '},
+    {' ',' ','X','X',' ',' '},
+    {' ',' ','X','X',' ',' '},
+    {' ',' ','X','X',' ',' '},
+    {' ',' ','X','X',' ',' '},
+    {' ','X','X','X','X',' '},
+
+};
+
+do
+{
+    Console.Clear();
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write(mas[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.Write("Включить лампочку? ");
+    string answer = Console.ReadLine();
+    switch (answer)
+    {
+        case ("да"):
+            {
+                for (int i = 0; i < mas.GetLength(0); i++)
+                {
+                    for (int j = 0; j < mas.GetLength(1); j++)
+                    {
+                        if (i == 1 && j == 2 || i == 1 && j == 3 || i == 2 && j == 1 || i == 2 && j == 2 ||
+                         i == 2 && j == 3 || i == 2 && j == 4) mas[i, j] = 'X';
+                    }
+                }
+            }
+            break;
+        case ("нет"):
+            {
+                for (int i = 0; i < mas.GetLength(0); i++)
+                {
+                    for (int j = 0; j < mas.GetLength(1); j++)
+                    {
+                        if (i == 1 && j == 2 || i == 1 && j == 3 || i == 2 && j == 1 || i == 2 && j == 2 ||
+                         i == 2 && j == 3 || i == 2 && j == 4) mas[i, j] = ' ';
+                    }
+                }
+            }
+            break;
+        default:break;
+    }
+}
+while (true);
+
+
+
+
+
+
+
