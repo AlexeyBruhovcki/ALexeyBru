b305ab10e43c2a5432d5550882dcd2ccf3be8e89
a a <doejayne20@gmail.com>
2024-05-12 16:33:09 +0200

    HW

---
+++ Theme17Lesson1/Program.cs
@@ -0,0 +1,36 @@
+﻿//Тема 17. Урок 1.
+Random random = new Random();
+int[][] mas = new int[random.Next(1, 11)][];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = new int[random.Next(1, 11)];
+}
+for (int i = 0; i < mas.Length; i++)
+{
+    for (int j = 0; j < mas[i].Length; j++)
+    {
+        mas[i][j] = random.Next(1, 10);
+        Console.Write(mas[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите число для замены случайного элемента: ");
+int num = int.Parse(Console.ReadLine());
+int k = 0;
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas[i].Length; j++)
+    {
+         if (k < 1)
+        {
+            int l = random.Next(mas.GetLength(0));
+            int m = random.Next(mas[l].Length);
+            mas[l][m] = num;
+            k++;
+        }
+        Console.Write(mas[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
---
+++ Theme17Lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Тема 17. Урок 2.
+Random random = new Random();
+int[][] mas = new int[3][];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = new int[random.Next(1, 11)];
+}
+int count = 0;
+double sumAll = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+    int sum = 0;
+    for (int j = 0; j < mas[i].Length; j++)
+    {
+
+        mas[i][j] = random.Next(1, 11);
+        sum += mas[i][j];
+        sumAll+= mas[i][j];
+        count++;
+        Console.Write(mas[i][j] + " ");
+    }
+    Console.Write("Сумма элементов одномерного массива"+" "+sum);
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.Write($"Среднее арифметическое всех элементов массива: {sumAll/count:F2}");
