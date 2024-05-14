f5aa87c4981251e5f5b7c473227bb7360f914542
a a <doejayne20@gmail.com>
2024-05-14 16:39:04 +0200

    HW

---
+++ Theme18Lesson1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Тема 18. Урок 1.
+Console.Write("Введите ключевые слова через пробел: ");
+string words= Console.ReadLine();
+string[] forbiddenWords = words.Split(' ');
+string sentence = "";
+do
+{
+    Console.Write("Введите слово для добавления в предложение: ");
+    string word = Console.ReadLine();
+    for (int i = 0; i < forbiddenWords.Length; i++)
+    {
+
+        if (forbiddenWords[i].ToLower() == word.ToLower())
+        {
+            word = "скрыто";
+            break;
+        }
+    }
+    if (word.ToLower() == "конец") break;
+    sentence += word+" ";
+}
+while(true);
+Console.Write("Сформированное предложение: "+(sentence));
---
+++ Theme18Lesson2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Тема 18. Урок 2.
+// Задача 1
+//Console.Write("Введите целые числа чере запятую: ");
+//string strNumbers=Console.ReadLine();
+//string[] masNumbers = strNumbers.Split(',');
+//int sum = 0;
+//foreach (string n in masNumbers)
+//{
+//    bool isNumber = true;
+//    for(int i = 0; i < n.Length; i++)
+//    {
+//        if (!char.IsDigit(n[i]))
+//        {
+//            isNumber = false;
+//            break;
+//        }
+//    }
+//    if (isNumber)
+//    {
+//        sum += Convert.ToInt32(n);
+//    }
+
+//}
+//Console.Write("Сумма чисел: "+(sum));
+
+// Задача 2
+Console.Write("Введите предложение: ");
+string sentence = Console.ReadLine();
+string[] words = sentence.Split(' ');
+int count = 0;
+foreach (string word in words)
+{
+    count++;
+}
+Console.Write("Количество слов: "+(count));
