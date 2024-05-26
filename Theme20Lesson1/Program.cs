06a44a116254db1cbedaefb63bc2435209c7cec9
a a <doejayne20@gmail.com>
2024-05-26 13:35:27 +0200

    HW

---
+++ Theme20Lesson1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Тема 20. Урок 1.
+Console.Write("Введите 1ое число: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите 2ое число: ");
+double b = double.Parse(Console.ReadLine());
+double Summ(double first, double second) 
+{
+    return first + second;
+}
+Console.Write($"Сумма чисел: {Summ(a,b):F2}");
---
+++ Theme20Lesson2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Тема 20. Урок 2.
+Random random = new Random();
+int[] mas = new int[random.Next(1, 20)];
+for(int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(1, 20);
+    Console.Write(mas[i]+" ");
+}
+double Average(int[] mas)
+{
+    double s = 0;
+    for(int i = 0; i < mas.Length; i++)
+    {
+        s += mas[i];
+    }
+    return s / mas.Length;
+}
+Console.WriteLine();
+Console.WriteLine($"Среднее арифметическое элементов массива: {Average(mas):F2}");
