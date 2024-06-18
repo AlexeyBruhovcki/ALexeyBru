2755aa122fdb8f98b616d63f1f7a755e426e8b72
a a <doejayne20@gmail.com>
2024-06-18 11:20:45 +0200

    HW

--- Theme22Lesson2/Program.cs
+++ Theme22Lesson2/Program.cs
@@ -1,30 +1,18 @@
 ﻿// Тема 22. Урок 2.
+
 try
 {
     Console.Write("Введите числа через пробел: ");
     string nums = Console.ReadLine();
     string[] splitNums = nums.Split(" ");
     int sum = 0;
-    foreach(string num in splitNums)
+    foreach (string num in splitNums)
     {
-        for(int i = 0; i < num.Length; i++)
-        {
-            if (!char.IsDigit(num[i]))
-            {
-                throw new FormatException("Введене строка.Требуется ввести число.");
-
-            }
-            if (char.IsDigit(num[i]))
-            {
-                sum += Convert.ToInt32(num);
-            }
-        }
-        
+      sum += Convert.ToInt32(num);
     }
-    Console.WriteLine("Сумма введенных чисел: "+sum);
+    Console.WriteLine("Сумма введенных чисел: " + sum);
 }
-catch(FormatException e)
+catch (FormatException e)
 {
     Console.WriteLine(e.Message);
 }
-
