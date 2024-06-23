4190ae47c66782935493adeef41736ff8589f1e6
a a <doejayne20@gmail.com>
2024-06-23 15:05:07 +0200

    HW

---
+++ Theme24Lesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Тема 24. Урок 1. Рекурсия.
+
+Console.Write("Введите слово или строку: ");
+string word = Console.ReadLine()!.ToLower();
+string wordChars = string.Concat(word.Where(char.IsLetterOrDigit));
+int lastIndex = wordChars.Length - 1;
+void IsPalindrom(string wwordChars, int curIndex, int lastIndex)
+{
+    if (wordChars[curIndex] != wordChars[lastIndex])
+    {
+        Console.WriteLine("Not a palindrom");
+        return;
+    }
+    else if (curIndex>=lastIndex)
+    {
+        Console.WriteLine("Is a palindrom");
+        return;
+    }
+    IsPalindrom(wordChars, curIndex + 1, lastIndex - 1);
+}
+IsPalindrom(wordChars, 0, lastIndex);
+
---
+++ Theme24Lesson2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Тема 24. Урок 2. рекурсия
+
+Console.Write("Введите число: ");
+int n;
+int.TryParse(Console.ReadLine(), out n);
+
+void DigtsUneven(int n)
+{
+    if (n <= 0) return;
+    DigtsUneven(n - 1);
+    if (n % 2 != 0)
+    {
+        Console.Write(n+" ");
+    }
+}
+
+DigtsUneven(n);
