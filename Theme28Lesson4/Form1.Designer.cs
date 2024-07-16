0155c6b44eef3e5b3a7e09eccd5cadbae8c9ae1d
a a <doejayne20@gmail.com>
2024-07-16 16:23:31 +0200

    HW

--- Theme28Lesson4/Form1.Designer.cs
+++ Theme28Lesson4/Form1.Designer.cs
@@ -30,6 +30,13 @@
         {
             listBox1 = new ListBox();
             label1 = new Label();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            textBoxPath = new TextBox();
+            label2 = new Label();
+            buttonSave = new Button();
+            buttonBowse = new Button();
             SuspendLayout();
             // 
             // listBox1
@@ -38,7 +45,7 @@
             listBox1.ItemHeight = 15;
             listBox1.Location = new Point(12, 33);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(251, 289);
+            listBox1.Size = new Size(307, 229);
             listBox1.TabIndex = 0;
             // 
             // label1
@@ -51,11 +58,55 @@
             label1.TabIndex = 1;
             label1.Text = "Список элементов";
             // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxPath
+            // 
+            textBoxPath.Location = new Point(12, 289);
+            textBoxPath.Name = "textBoxPath";
+            textBoxPath.Size = new Size(307, 23);
+            textBoxPath.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 265);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Введите путь к файлу:";
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(93, 318);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(75, 23);
+            buttonSave.TabIndex = 4;
+            buttonSave.Text = "В список";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // buttonBowse
+            // 
+            buttonBowse.Location = new Point(12, 318);
+            buttonBowse.Name = "buttonBowse";
+            buttonBowse.Size = new Size(75, 23);
+            buttonBowse.TabIndex = 5;
+            buttonBowse.Text = "Обзор";
+            buttonBowse.UseVisualStyleBackColor = true;
+            buttonBowse.Click += buttonBowse_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(525, 479);
+            ClientSize = new Size(334, 352);
+            Controls.Add(buttonBowse);
+            Controls.Add(buttonSave);
+            Controls.Add(label2);
+            Controls.Add(textBoxPath);
             Controls.Add(label1);
             Controls.Add(listBox1);
             Name = "Form1";
@@ -68,5 +119,12 @@
 
         private ListBox listBox1;
         private Label label1;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TextBox textBoxPath;
+        private Label label2;
+        private Button buttonSave;
+        private Button buttonBowse;
     }
 }
--- Theme28Lesson4/Form1.cs
+++ Theme28Lesson4/Form1.cs
@@ -5,6 +5,48 @@ namespace Theme28Lesson4
         public Form1()
         {
             InitializeComponent();
+
+        }
+        private void LisboxOperator(string[] text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                listBox1.Items.Add(text[i]);
+            }
+        }
+        private string[] StringToArray(string text) => text.Split("\n");
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            try
+            {
+                FileInfo info = new FileInfo(textBoxPath.Text);
+                StreamReader reader = info.OpenText();
+                {
+                    string text = reader.ReadToEnd();
+                    LisboxOperator(StringToArray(text));
+                }
+
+                //string text = File.ReadAllText(textBoxPath.Text);
+                //LisboxOperator(StringToArray(text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonBowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath.Text = dialog.FileName;
+            }
+
         }
     }
 }
