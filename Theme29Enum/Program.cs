90e715104baef54dbbdc9388c58424c30c4345e0
a a <doejayne20@gmail.com>
2024-07-16 16:51:23 +0200

    HW

---
+++ Theme29Enum/Program.cs
@@ -0,0 +1,8 @@
+﻿
+for(int i = 0; i < 12; i++)
+{
+    string? month = Enum.GetName(typeof (Months),i);
+    Console.WriteLine($"Выбран месяц: {month}, порядковый номер {i+1}");
+}
+
+enum Months { January,February,March,April,May,June,July,August,Septemper,October,November,December}
